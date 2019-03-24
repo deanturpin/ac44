85033528469a3ac0b57c1f9409f01d31a0dc75b8
Definition of Dean <defofdean@gmail.com>
2019-03-24 22:02:31 +0000

    Use uint32_t and dump some stats

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -5,28 +6,35 @@
 int main() {
 
   struct wav_header {
-    using word = unsigned int;
-    word riff_id;
-    word riff_size;
-    word wave_tag;
-    word format_id;
-    word format_size;
-    word format_tag : 16;
-    word channels : 16;
-    word sample_rate;
-    word bytes_per_second;
-    word block_align : 16;
-    word bit_depth : 16;
-    word data_id;
-    word data_size;
+    uint32_t riff_id;
+    uint32_t riff_size;
+    uint32_t wave_tag;
+    uint32_t format_id;
+    uint32_t format_size;
+    uint32_t format_tag : 16;
+    uint32_t channels : 16;
+    uint32_t sample_rate;
+    uint32_t bytes_per_second;
+    uint32_t block_align : 16;
+    uint32_t bit_depth : 16;
+    uint32_t data_id;
+    uint32_t data_size;
   };
+
+  assert(sizeof(wav_header) == 44);
 
   std::cout << "audio read\n";
   const std::string file{"../dft/wav/hobgoblin_didge_8000.wav"};
 
-  if (std::ifstream in{file}; in.good())
+  if (std::ifstream in{file}; in.good()) {
     std::cout << file << " is good\n";
 
-  std::ifstream in{"notafile"};
-  std::cout << "file is " << (in.good() ? "good" : "bad") << '\n';
+    wav_header h;
+    in.read(reinterpret_cast<char *>(&h), sizeof h);
+
+    std::cout << std::hex << std::showbase << h.riff_id << '\n'
+              << h.riff_size << '\n'
+              << h.wave_tag << '\n'
+              << std::dec << h.data_size << " bytes\n";
+  }
 }
