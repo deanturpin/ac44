f841f5492c4f91a7d0609c43a6e3e38d3d374108
Definition of Dean <defofdean@gmail.com>
2019-03-24 21:52:00 +0000

    Add WAV header

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
-
 #include <fstream>
 #include <iostream>
 #include <string>
 
 int main() {
-  std::cout << "audio read\n";
 
+  struct wav_header {
+    using word = unsigned int;
+    word riff_id;
+    word riff_size;
+    word wave_tag;
+    word format_id;
+    word format_size;
+    word format_tag : 16;
+    word channels : 16;
+    word sample_rate;
+    word bytes_per_second;
+    word block_align : 16;
+    word bit_depth : 16;
+    word data_id;
+    word data_size;
+  };
+
+  std::cout << "audio read\n";
   const std::string file{"../dft/wav/hobgoblin_didge_8000.wav"};
 
   if (std::ifstream in{file}; in.good())
     std::cout << file << " is good\n";
+
+  std::ifstream in{"notafile"};
+  std::cout << "file is " << (in.good() ? "good" : "bad") << '\n';
 }
