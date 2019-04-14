fc8e8cda02fdec21de83b7c9daf504fc83c662b9
Definition of Dean <defofdean@gmail.com>
2019-04-14 20:12:07 +0100

    Dump header first

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ using iterator_t = const std::vector<sample_t>::const_iterator;
 std::string create_display_histogram(iterator_t &begin, iterator_t &end) {
 
   // Create display histogram
-  const size_t bin_count{36};
+  const size_t bin_count{37};
   const size_t bin_width{std::distance(begin, end) / bin_count};
   std::map<size_t, uint32_t> hist;
 
@@ -94,14 +94,13 @@ int main() {
     const auto &[min, max] =
         std::minmax_element(std::cbegin(samples), std::cend(samples));
 
-    // Report histogram
-    std::cout << create_display_histogram(std::cbegin(samples),
-                                          std::cend(samples));
-
     // Stop timer and report stats
     const auto end  = std::chrono::system_clock::now();
     const auto diff = duration_cast<milliseconds>(end - start);
     std::cout << dump_meta(meta) << *min << '\t' << *max << '\t' << diff.count()
-              << " us\n";
+              << " us\n"
+              << create_display_histogram(std::cbegin(samples),
+                                          std::cend(samples));
+    ;
   }
 }
