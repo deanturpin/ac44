b34b3f4e75551c7d027a90bfd533921f079c89a4
Definition of Dean <defofdean@gmail.com>
2019-04-15 22:03:50 +0100

    Rename meta routine to reflect return value

--- main.cpp
+++ main.cpp
@@ -12,34 +12,33 @@
 #include <string>
 #include <vector>
 
-// Structure of a WAV
-struct ac44 {
-  uint32_t riff_id{};
-  uint32_t riff_size{};
-  char wave_tag[4]{};
-  uint32_t format_id{};
-  uint32_t format_size{};
-  uint16_t format_tag{};
-  uint16_t channels{};
-  uint32_t sample_rate{};
-  uint32_t bytes_per_second{};
-  uint16_t block_align{};
-  uint16_t bit_depth{};
-  uint32_t data_id{};
-  uint32_t data_size{};
-};
+// Read WAV header from a stream and return sample rate
+uint32_t get_sample_rate(std::istream &in) {
 
-// Read WAV header from a stream
-auto get_meta(std::istream &in) {
+  // Structure of a WAV
+  struct ac44 {
+    uint32_t riff_id{};
+    uint32_t riff_size{};
+    char wave_tag[4]{};
+    uint32_t format_id{};
+    uint32_t format_size{};
+    uint16_t format_tag{};
+    uint16_t channels{};
+    uint32_t sample_rate{};
+    uint32_t bytes_per_second{};
+    uint16_t block_align{};
+    uint16_t bit_depth{};
+    uint32_t data_id{};
+    uint32_t data_size{};
+  } meta;
 
-  ac44 meta;
   assert(sizeof(meta) == 44);
 
   // Check file is good and read the meta data
   if (in.good())
     in.read(reinterpret_cast<char *>(&meta), sizeof meta);
 
-  return meta;
+  return meta.sample_rate;
 }
 
 // Return a simple histogram of the samples passed in
@@ -83,11 +82,11 @@ std::string create_display_histogram(iterator_t &begin, iterator_t &end) {
 int main() {
 
   // Read the header
-  auto &in         = std::cin;
-  const auto &meta = get_meta(in);
+  auto &in                = std::cin;
+  const auto &sample_rate = get_sample_rate(in);
 
-  // Read batches of s
-  std::vector<sample_t> s(meta.sample_rate);
+  // Read batches of samples
+  std::vector<sample_t> s(sample_rate);
   while (in.read(reinterpret_cast<char *>(s.data()),
                  s.size() * sizeof(sample_t))) {
 
