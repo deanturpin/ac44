b62db80f11c7726914d1df0ced88b695b250de1b
Definition of Dean <defofdean@gmail.com>
2019-04-16 09:22:01 +0100

    Refactor reporting mechanism

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include <algorithm>
-#include <bitset>
 #include <cassert>
-#include <chrono>
 #include <cmath>
-#include <fstream>
 #include <iostream>
-#include <iterator>
-#include <map>
 #include <numeric>
-#include <sstream>
 #include <string>
 #include <vector>
 
@@ -41,44 +35,6 @@ uint32_t get_sample_rate(std::istream &in) {
   return meta.sample_rate;
 }
 
-// Return a simple histogram of the samples passed in
-using sample_t   = int16_t;
-using iterator_t = const std::vector<sample_t>::const_iterator;
-std::string create_display_histogram(iterator_t &begin, iterator_t &end) {
-
-  // Create display histogram
-  const size_t bin_count{41};
-  const size_t bin_width{std::distance(begin, end) / bin_count};
-  std::map<size_t, uint64_t> hist;
-
-  // Populate display histogram by truncating the pulse index to fit it on
-  // the screen
-  for (auto i = begin; i != end; ++i)
-    hist[std::distance(begin, i) / bin_width] += std::abs(*i);
-
-  // Calculate max histogram bin to scale the display
-  const auto peak = std::max_element(std::cbegin(hist), std::cend(hist),
-                                     [](const auto &a, const auto &b) {
-                                       return a.second < b.second;
-                                     })
-                        ->second;
-
-  // Report histogram
-  std::stringstream out;
-  for (const auto &[bin, value] : hist) {
-
-    // Calculate bar length for this bin
-    const size_t max_bar_length = 70;
-    const size_t bar_length     = max_bar_length * value / peak;
-
-    // Construct histogram bar and mark if it's clipped, always add one so we
-    // don't attempt to constrcut a zero length string
-    out << std::string(1 + bar_length, '-') << '\n';
-  }
-
-  return out.str();
-}
-
 int main() {
 
   // Read the header
@@ -86,33 +42,34 @@ int main() {
   const auto &sample_rate = get_sample_rate(in);
 
   // Read batches of samples
+  using sample_t = int16_t;
   std::vector<sample_t> s(sample_rate);
   while (in.read(reinterpret_cast<char *>(s.data()),
                  s.size() * sizeof(sample_t))) {
 
-    using clock = std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::microseconds;
+    // Report summary of batch by splitting into blocks
+    const size_t x = 4000;
+    for (auto i = std::cbegin(s); i < std::prev(std::cend(s), x); i += x) {
+      const auto average_amplitude =
+          std::accumulate(
+              i, std::next(i, x), 0,
+              [](auto sum = 0, const auto &a) { return sum += std::abs(a); }) /
+          x;
 
-    // Start timer
-    const auto start = clock::now();
+      // Start and end for current block
+      const auto begin = i;
+      const auto end   = std::prev(std::cend(s), x);
 
-    // Calculate peak sample
-    const auto peak = std::abs(*std::max_element(
-        std::cbegin(s), std::cend(s), [](const auto &a, const auto &b) {
-          return std::abs(a) < std::abs(b);
-        }));
+      // Find the peak so we can scale the output
+      uint32_t max_so_far = *std::max_element(begin, end);
 
-    // Calculate bit template
-    sample_t id{};
-    std::for_each(std::cbegin(s), std::cend(s),
-                  [&](const auto &sample) { id |= std::abs(sample); });
+      // Calculate bar length for this bin
+      const size_t max_bar_length = 65;
+      const size_t bar_length = max_bar_length * average_amplitude / max_so_far;
 
-    // Stop timer and report stats
-    const auto end  = clock::now();
-    const auto diff = duration_cast<microseconds>(end - start);
-    std::cout << peak << '\t' << std::bitset<16>(id) << '\t' << diff.count()
-              << " us\n"
-              << create_display_histogram(std::cbegin(s), std::cend(s));
+      // Construct histogram bar and mark if it's clipped, always add one so we
+      // don't attempt to constrcut a zero length string
+      std::cout << std::string(1 + bar_length, '-') << '|' << '\n';
+    }
   }
 }
