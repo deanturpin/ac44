8c84f3d0b186e3f324dddfd34cfcafdb43c5f950
Definition of Dean <defofdean@gmail.com>
2019-04-21 16:16:44 +0100

    Revert to original twiddle calc (no quicker for breaking out)

--- fourier.cpp
+++ fourier.cpp
@@ -9,26 +9,15 @@ auto fourier_init() {
 
   _timestamp(__LINE__, __FILE__);
 
-  // const size_t bins{fourier_bins};
-
   // Declare container for twiddle matrix
   std::vector<std::complex<double>> twiddle;
   twiddle.reserve(bins<size_t> * bins<size_t> / 2);
 
   // Populate twiddle matrix
   using namespace std::complex_literals;
-  const auto two_i_times_pi = 2i * pi<double>;
-
-  for (size_t k = 0; k < bins<size_t> / 2; ++k) {
-    const double K          = k;
-    const auto imag_times_k = two_i_times_pi * K;
-
-    for (size_t n = 0; n < bins<size_t>; ++n) {
-
-      const double N = n;
-      twiddle.push_back(exp(imag_times_k * N / bins<double>));
-    }
-  }
+  for (size_t k = 0; k < bins<size_t> / 2; ++k)
+    for (size_t n = 0; n < bins<size_t>; ++n)
+      twiddle.push_back(exp(2i * M_PI * double(k) * double(n) / bins<double>));
 
   _timestamp(__LINE__, __FILE__);
 
@@ -45,7 +34,6 @@ std::vector<double> get_fourier(const std::vector<sample_t> &samples) {
   static const auto &twiddle = fourier_init();
 
   // Initialise results container and reserve enough space for the bins
-  // const size_t bins{fourier_bins};
   std::vector<double> fourier;
   fourier.reserve(bins<size_t>);
 
