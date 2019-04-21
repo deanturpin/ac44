48872f8b87fefc40ec5c19a89c8a9e3e2b26c37c
Definition of Dean <defofdean@gmail.com>
2019-04-21 17:59:37 +0100

    Break scaling out into own routine

--- ascii.cpp
+++ ascii.cpp
@@ -1,6 +1,7 @@
 #include "ascii.h"
 #include <algorithm>
 #include <cmath>
+#include <functional>
 #include <sstream>
 
 // Create text representation of basic histogram
@@ -22,31 +23,45 @@ std::string dump_histogram(const iterator_t &begin, const iterator_t &end) {
   return out.str();
 }
 
-// Create text representation of histogram with a log X axis
-std::string dump_log_histogram(const iterator_t &begin, const iterator_t &end) {
+// Routine that takes a histogram and a function to scale it and returns the
+// scaled histogram
+using cont_t = std::vector<double>;
+cont_t scale_histogram(const iterator_t &begin, const iterator_t &end,
+                       std::function<size_t(size_t)> func) {
 
-  // Construct log histogram
-  std::vector<double> log_histogram;
-  log_histogram.reserve(std::distance(begin, end));
+  // Construct scaled histogram
+  std::vector<double> scaled_histogram;
+  scaled_histogram.reserve(std::distance(begin, end));
 
   std::for_each(begin, end, [&, n = 0](const auto &s) mutable {
-    size_t index = std::rint(std::log2(n * 1024 * 1024));
+    // size_t index = std::rint(std::log2(n * 1024 * 1024));
+    size_t index = func(n);
 
     // If it's a new bin create it
-    if (index >= log_histogram.size())
-      log_histogram.emplace_back();
+    if (index >= scaled_histogram.size())
+      scaled_histogram.emplace_back();
 
     // Sum all the values that match a bin
-    log_histogram.back() += s;
+    scaled_histogram.back() += s;
 
     ++n;
   });
 
   // Free up any unused memory
-  log_histogram.shrink_to_fit();
+  scaled_histogram.shrink_to_fit();
+
+  return scaled_histogram;
+}
+
+// Create text representation of histogram with a log X axis
+std::string dump_log_histogram(const iterator_t &begin, const iterator_t &end) {
+
+  const auto scaled = scale_histogram(begin, end, [](const auto &n) {
+    return std::rint(std::log2(n * 1024 * 1024));
+  });
 
   // Pass log histogram to basic histogram routine
-  return dump_histogram(std::cbegin(log_histogram), std::cend(log_histogram));
+  return dump_histogram(std::cbegin(scaled), std::cend(scaled));
 }
 
 // Create text representation of log histogram from above
@@ -54,24 +69,9 @@ std::string dump_aerial_histogram(const iterator_t &begin,
                                   const iterator_t &end) {
 
   // Construct scaled histogram
-  std::vector<double> scaled;
-  scaled.reserve(std::distance(begin, end));
-
-  std::for_each(begin, end, [&, n = 0](const auto &s) mutable {
-    size_t index = std::rint(85.0 * n / std::distance(begin, end));
-
-    // If it's a new bin create it
-    if (index >= scaled.size())
-      scaled.emplace_back();
-
-    // Sum all the values that match a bin
-    scaled.back() += s;
-
-    ++n;
+  const auto scaled = scale_histogram(begin, end, [&](const auto &n) {
+    return std::rint(85.0 * n / std::distance(begin, end));
   });
-
-  // Free up any unused memory
-  scaled.shrink_to_fit();
 
   // Max length of a bar
   const std::string chars{" _.oO#"};
